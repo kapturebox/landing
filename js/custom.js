7c55fe7d2c1a24eeab25b398d4a9e9f81c603a3f
kimbergee <kimber.gee@gmail.com>
2017-08-15 14:24:26 -0400

    move particles script to js file

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
-  var isOpen = false;
-  var insideClick;
+  // load particles
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles', 'assets/particles.json');
+
 
   // mobile toggle menu
   $("#toggle-bars, #menu li").click(function() {
@@ -11,6 +13,9 @@ $(document).ready(function() {
 
 
   // make mobile menu close if clicked outside of it
+  var isOpen = false;
+  var insideClick;
+  
   $(document, "#toggle-bars, #menu").on('click', function() {
     var eventId = $(event.target).closest('#toggle-bars, #menu').attr('id');
 
